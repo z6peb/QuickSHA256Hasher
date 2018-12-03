8318ce237c85ccbaeeb04599345a22cedc0cb870
Z6PEB <peb@kmd.dk>
2018-12-03 12:58:16 +0100

    input changed to file

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.cert.Certificate;
@@ -10,10 +11,23 @@ public class App {
     public static void main(String[] args) throws IOException {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         while (true) {
-            System.out.println("Enter a certificate: ");
-            String payload = reader.next(); // Scans the next token of the input as an int.
+            System.out.println("Enter the path of a certificate (.cer) file: ");
 
+            // pass the path to the file as a parameter
+            File file = new File(reader.nextLine());
+
+            Scanner sc = null;
             try {
+                sc = new Scanner(file);
+
+                String payload = "";
+                while (sc.hasNextLine()) {
+                    String nLine = sc.nextLine();
+                    if (nLine.trim().startsWith("-----"))
+                        continue;
+                    payload += nLine;
+                }
+
                 MessageDigest digest = MessageDigest.getInstance("SHA-256");
 
                 // verify payload string is valid X.509 certificate
@@ -21,13 +35,20 @@ public class App {
                 Certificate cert = cf.generateCertificate(new ByteArrayInputStream(Base64.getDecoder().decode(payload)));
 
                 byte[] hash = digest.digest(cert.getEncoded());
-                System.out.println( javax.xml.bind.DatatypeConverter.printHexBinary(hash).toLowerCase() );
+                System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(hash).toLowerCase());
 
             } catch (Exception e) {
                 System.out.println("Failed to init");
+            } finally {
+                try {
+                    sc.close();
+                } catch (Exception e) {
+                    System.out.println("Failed to close file");
+                }
             }
         }
         //once finished
         //reader.close();
     }
 }
+
